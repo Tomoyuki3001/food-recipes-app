00ee9df1f25ada5d1e58d3b77a150edfe433e3cc
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-11-12 12:19:44 -0800

    Added the recipe card and the detail page

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,10 +1,14 @@
 export async function fetchRecipes(filter) {
   const { query, limit } = filter;
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}&from=0&to=${limit}&`;
-
   const response = await fetch(url);
-
   const data = await response.json();
-  console.log("data", data);
   return data?.hits;
+}
+
+export async function fetchRecipe(id) {
+  const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.recipe;
 }
