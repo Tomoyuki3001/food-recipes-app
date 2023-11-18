feb4b9d41114377114404c769b58768fe9b84545
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-11-18 10:13:16 -0800

    Fixed the API

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,6 +1,7 @@
 export async function fetchRecipes(filter) {
   const { query, limit } = filter;
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}&from=0&to=${limit}&`;
+  // const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}&from=0&to=${limit}&`;
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=84381437&app_key=f6adb2d0f0afad645b56f4f0ecffeb8c`;
   const response = await fetch(url);
   const data = await response.json();
   return data?.hits;
