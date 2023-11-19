ee97ad03218fb4a98b1487760f897fb53a688655
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-11-18 22:59:17 -0800

    Removed some codes

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,9 +1,6 @@
 export async function fetchRecipes(filter) {
-  const { query, limit } = filter;
-  const apiId = process.env.REACT_APP_EDAMAM_API_ID;
-  const apiKey = process.env.REACT_APP_EDAMAM_API_KEY;
-  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${apiId}&app_key=${apiKey}&from=0&to=${limit}&`;
-  // const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=84381437&app_key=f6adb2d0f0afad645b56f4f0ecffeb8c`;
+  const query = filter;
+  const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}`;
   const response = await fetch(url);
   const data = await response.json();
   return data?.hits;
@@ -11,7 +8,6 @@ export async function fetchRecipes(filter) {
 
 export async function fetchRecipe(id) {
   const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=${process.env.REACT_APP_EDAMAM_API_ID}&app_key=${process.env.REACT_APP_EDAMAM_API_KEY}`;
-  // const url = `https://api.edamam.com/api/recipes/v2/${id}?type=public&app_id=84381437&app_key=f6adb2d0f0afad645b56f4f0ecffeb8c`;
   const response = await fetch(url);
   const data = await response.json();
   return data.recipe;
